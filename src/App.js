365c7fc32bab7212c7c7b7d971c5ceaf935b0efc
Filippos Kontoes <fil13kon@gmail.com>
2022-09-10 16:12:18 +0300

    set libs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Title>TEST</Title>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
