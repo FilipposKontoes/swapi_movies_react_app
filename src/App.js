c4c3ac150453c1c3398c575d0fb3a22c038e506c
Filippos Kontoes <fil13kon@gmail.com>
2022-09-13 00:31:17 +0300

    spinner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import useData from "./Hooks/useData";
+import { ColorRing } from 'react-loader-spinner'
 import {useEffect, useState} from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import ListItem from "./Components/ListItem/ListItem";
@@ -27,7 +28,8 @@ function App() {
       <NavBar userInput={userInput} setUserInput={setUserInput} sortByEpisode={sortByEpisode} sortByYear={sortByYear} />
       <MainContentWrapper>
         <ListItemsWrapper>
-            {data?.filter(filterMovies).map((movie) => {
+            {isLoading ? <ColorRing/> :
+            data?.filter(filterMovies).map((movie) => {
                 return (<ListItem key={movie.episode_id} movie={movie} setSelectedMovie={setSelectedMovie} />)
             })}
         </ListItemsWrapper>
