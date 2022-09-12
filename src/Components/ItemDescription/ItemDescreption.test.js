f07ea91c21b1cb48f5f8e91237197ecd9970b607
Filippos Kontoes <fil13kon@gmail.com>
2022-09-13 01:34:04 +0300

    first tests!

--- src/Components/ItemDescription/ItemDescreption.test.js
+++ src/Components/ItemDescription/ItemDescreption.test.js
@@ -7,8 +7,11 @@ test('It renders correctly when there is no selected movie', () => {
     expect(screen.getByTestId('noMovieDescription')).toBeInTheDocument()
 })
 test('It renders correctly when there is selected movie', () => {
-    const selectedMovie = [];
+    const selectedMovie = {'director': '1', 'title': '2', 'opening_crawl': '3' };
     render(<ItemDescription selectedMovie={selectedMovie}/>);
-    expect(screen.getByTestId('movieDescription')).toBeInTheDocument()
+    expect(screen.getByTestId('movieDescription')).toBeInTheDocument();
+    expect(screen.getByText('Directed by: 1')).toBeInTheDocument();
+    expect(screen.getByText('2')).toBeInTheDocument();
+    expect(screen.getByText('3')).toBeInTheDocument();
 })
 })
